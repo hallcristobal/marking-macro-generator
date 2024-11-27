c9f572035abf928614732ccda0809af058c29d0c
Cristobal Hall-Ramos <crishallramos@yahoo.com>
2024-11-26 23:50:41 -0500

    pages

--- src/index.js
+++
@@ -1,9 +0,0 @@
-const React = require("react")
-const { createRoot } = require("react-dom/client")
-const Generator = require("./generator").default;
-
-createRoot(document.getElementById("root")).render(
-    <React.StrictMode>
-        <Generator />
-    </React.StrictMode>
-);
